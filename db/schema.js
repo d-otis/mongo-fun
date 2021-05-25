3a1c306ef4dca2199e5507b65dadc8d6756b2eca
d-otis <dan@dan-foley.com>
2021-05-25 19:29:50 -0400

    mongo hello world

---
+++ db/schema.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost:27017/blog_demo", {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  body: String
+})
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  posts: [postSchema]
+})
+
+const Post = new mongoose.model("Post", postSchema)
+const User = new mongoose.model("User", userSchema)
+
+let danny = new User({
+  name: "Danny",
+  email: "dan.dorff@gmail.com"
+})
+
+db.on("error", console.error.bind(console, "connection error: "))
+// db.once("open", () => {
+//   console.log('connected')
+//   let danny = new User({
+//     name: "Danny",
+//     email: "dan.orff@gmail.com"
+//   })
+
+//   danny.save((err, user) => {
+//     if(err) {
+//       console.log(err)
+//     } else {
+//       console.log(user)
+//     }
+//   })
+// })
+
+
+
+console.log('hiiii')
